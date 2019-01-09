be336961788a8f46b3b3b53cfcd691aedf3aa167
brooke perry and Robert Bruce <brooke@>
2019-01-09 11:33:08 -0800

    refactor 1st spec to check all obj props, add 2nd spec check for numbers

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -2,24 +2,46 @@ import { Haiku } from './../src/haiku.js';
 
 describe('Haiku', function() {
   let haiku;
+  let badHaiku;
 
   beforeEach(function() {
-    haiku = new Haiku("Unicorn", "Sara");
+    haiku = new Haiku(
+              "Unicorn",
+              "Sara",
+              "This",
+              "These",
+              "Those"
+            );
+
+    badHaiku = new Haiku(
+                  "Ke5ha",
+                  "K35ha",
+                  "This",
+                  "These",
+                  "Those"
+    );
 
   })
 
-  it('should test that Haiku has a title', function() {
+  it('should test that Haiku has all properties', function() {
     expect(haiku.title).toEqual("Unicorn");
     expect(haiku.author).toEqual("Sara");
-  });
-
-  it('should test that Haiku has 3 lines', function() {
-    haiku.line1 = "This";
-    haiku.line2 = "These";
-    haiku.line3 = "Those";
     expect(haiku.line1).toEqual("This");
     expect(haiku.line2).toEqual("These");
     expect(haiku.line3).toEqual("Those");
   });
+
+  it('should test that Haiku properties only include letters and punctuation.', function() {
+    expect(badHaiku.validInput()).toEqual(false);
+    expect(haiku.validInput()).toEqual(true);
+    expect(haiku.validInput()).not.toEqual(false);
+
+  });
+
+  // it('should test that Haiku has a title', function() {
+  //   expect(haiku.title).toEqual("Unicorn");
+  //   expect(haiku.author).toEqual("Sara");
+  // });
+
 
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,8 +1,22 @@
 export class Haiku {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
+  constructor(title, author, line1, line2, line3) {
+    title.match(/[0-9]/gm) ? null : this.title = title;
+    author.match(/[0-9]/gm) ? null : this.author = author;
+    line1.match(/[0-9]/gm) ? null : this.line1 = line1;
+    line2.match(/[0-9]/gm) ? null : this.line2 = line2;
+    line3.match(/[0-9]/gm) ? null : this.line3 = line3;
   }
 
+  validInput() {
+    let result = true;
+
+    this.title == null ? result = false: null;
+    this.author == null ? result = false: null;
+    this.line1 == null ? result = false: null;
+    this.line2 == null ? result = false: null;
+    this.line3 == null ? result = false: null;
+
+    return result;
+  }
 
 }
